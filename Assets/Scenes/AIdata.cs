ba79cc84623127b9498355c60822eb1329ca6729
jooyeokkim <67864645+jooyeokkim@users.noreply.github.com>
2020-07-14 10:59:21 +0900

    aldata

---
+++ Assets/Scenes/AIdata.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIdata : MonoBehaviour
+{
+    public static AIdata instance = null;
+    public int count;
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        count = 0;
+        up();
+    }
+    void up()
+    {
+        count++;
+        Invoke("up", 2);
+    }
+}
